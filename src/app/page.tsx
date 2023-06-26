8ef7784b5097f6485f6cb96da8e6099cdc1d54d7
Lucho Morales Segobia <luchoms09@gmail.com>
2023-06-26 16:30:54 -0500

    chore: add meta data to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+import { type Metadata } from 'next';
+
 import { Banner, Hero, Products } from '@/components/Home';
+
+export const metadata: Metadata = {
+  title: 'PoC Chat Retail',
+  description:
+    'Prueba de Concepto para el Laboratorio de AI, creando un asistente Retail',
+};
 
 export default function Home() {
   return (
